0f0ed510230163f60ae2a1a2d0979b2d8b6a462d
gaspar1986 <491028246@qq.com>
2016-12-06 17:38:30 +0800

    add props

--- 4.props.js
+++ 4.props.js
@@ -4,14 +4,21 @@
  * 3.属性一般由父组件使用子组件时候传入的
  * 4.this代表当前组件实例
  */
+var style = {
+    color:'red'
+}
 var Person = React.createClass({
     render(){
-        return <div>
+        return <div style={style}>
             {this.props.name}
         </div>
     }
 })
 ReactDOM.render(
-    <Person gender="男" name="张三"/>,
+    <div>
+        <Person gender="男" name="张三"/>
+        <Person gender="男" name="李四"/>
+        <Person gender="男" name="王五"/>
+    </div>,
     document.getElementById('app')
 );

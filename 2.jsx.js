8057dd9f62b1438cd1d413ff5ffae7b946fee0bf
gaspar1986 <491028246@qq.com>
2016-12-06 16:28:40 +0800

    add

---
+++ 2.jsx.js
@@ -0,0 +1,4 @@
+var persons = ['张三','李四','王五'];
+ReactDOM.render(
+
+    ,document.getElementById('app'));
---
+++ index.js
@@ -0,0 +1,3 @@
+ReactDOM.render(<div>
+    <h1>你好</h1>
+</div>,document.getElementById('app'));
